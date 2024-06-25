66d00d8a847af915b09968698d8060a94ec89a38
Percival <emperorsheevpalpatine0@gmail.com>
2024-06-25 23:52:21 +0800

    feat: implement LLM prompt chain with LCEL documentation and user question handling - added code tool and parser for LangChain expression language (LCEL). - implemented a new LLM prompt chain with LCEL documentation context and user question handling.

--- code-assistant/app/chain.py
+++ code-assistant/app/chain.py
@@ -21,4 +21,66 @@ concatenated_content = "\n\n\n --- \n\n\n".join(
     [doc.page_content for doc in d_reversed]
 )
 
-print(concatenated_content)
+
+from operator import itemgetter
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain_core.output_parsers.openai_tools import PydanticToolsParser
+from langchain_core.utils.function_calling import convert_to_openai_tool
+
+## Data model
+class code(BaseModel):
+    """Code output"""
+    prefix: str = Field(description="Description of the problem and approach")
+    imports: str = Field(description="Code block import statements")
+    code: str = Field(description="Code block not including import statements")
+
+## LLM
+model = ChatOpenAI(temperature=0, streaming=True)
+
+## Tool
+code_tool_oai = convert_to_openai_tool(code)
+
+## LLM with tool and enforce invocation
+llm_with_tool = model.bind(
+    tools=[code_tool_oai],
+    tool_choice={"type": "function", "function": {"name": "code"}},
+)
+
+# Parser
+parser_tool = PydanticToolsParser(tools=[code])
+
+## Prompt
+template = """
+You are a coding assistant with expertise in LCEL, LangChain expression language. 
+Here is a full set of LCEL documentation: 
+\n _______ \n 
+{context} 
+\n _______ \n 
+Answer the user question based on the above provided documentation. 
+Ensure any code you provide can be executed with all required imports and variables defined. 
+Structure your answer with a description of the code solution. 
+Then list the imports. And finally list the functioning code block. 
+Here is the user question: \n _______ \n {question}
+"""
+
+## Prompt
+prompt = PromptTemplate(
+    template=template,
+    input_variables=["context", "question"],
+)
+
+# Chain
+chain = (
+    {
+        "context": lambda x: docs,
+        "context": lambda x: concatenated_content,
+        "question": itemgetter("question"),
+    }
+    | prompt
+    | llm_with_tool
+    | parser_tool
+)
+
+chain.invoke({"question": "How to create a RAG chain in LCEL?"})
